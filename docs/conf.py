84ded51eb2c925ef0f964a537577e816df8fce63
Miyoung Choi <myoung8739@gamil.com>
2021-06-07 15:31:50 -0700

    build test

--- docs/conf.py
+++ docs/conf.py
@@ -63,12 +63,33 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# List of directories, relative to source directory, that shouldn't be searched
+# for source files.
+exclude_trees = ["_build"]
+
+pygments_style = "default"
+
+autoclass_content = "both"
+autodoc_default_flags = ["show-inheritance", "members", "undoc-members"]
+autodoc_member_order = "bysource"
+
+
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+    "includehidden": True,
+    "titles_only": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
