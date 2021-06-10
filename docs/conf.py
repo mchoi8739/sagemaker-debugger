92f97db55549a1c7998f3e9a66f0935ba173ab10
Miyoung Choi <myoung8739@gamil.com>
2021-06-09 17:53:27 -0700

    try adding a path

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,11 @@ import os
 import sys
 
 import smdebug
-import smdebug.trials
 #sys.path.insert(0, os.path.abspath('../'))
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 sys.path.insert(0, os.path.join(curr_path, '..'))
 sys.path.insert(0, os.path.abspath('../smdebug'))
+sys.path.insert(0, os.path.abspath('../smdebug/trials'))
 
 # -- Project information -----------------------------------------------------
 
