11e6d0c68b755741029fc1f5362fac00b0f183d3
Miyoung Choi <myoung8739@gamil.com>
2021-06-09 17:45:35 -0700

    test cof path

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ import smdebug
 #sys.path.insert(0, os.path.abspath('../'))
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 sys.path.insert(0, os.path.join(curr_path, '..'))
+sys.path.insert(0, os.path.join(curr_path, '../..'))
 sys.path.insert(0, os.path.abspath('../smdebug'))
 
 # -- Project information -----------------------------------------------------
