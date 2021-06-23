03b1f6663bb1bfb5dc0bf62ee5da7d9f7d0e8ac2
Miyoung Choi <myoung8739@gamil.com>
2021-06-23 12:31:24 -0700

    test

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import smdebug
 
 #sys.path.insert(0, os.path.abspath('../'))
-curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+#curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 #sys.path.insert(0, os.path.join(curr_path, '..'))
 sys.path.insert(0, os.path.abspath('../smdebug'))
 
